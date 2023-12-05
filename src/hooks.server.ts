10af0621c9aa0ec1188a601d233df3cb424dd68d
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-12-05 22:26:07 +0100

    Use access token to retrieve and pass user details to client-code

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,35 @@
 import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
-	const email = event.cookies.get('session');
+	const accessToken = event.cookies.get('session');
 
-	// Add the user's email to the request
-	if (email) event.locals.user = { email };
+	// If there's no access token, just resolve the request as normal
+	if (typeof accessToken !== 'string' || !accessToken) return await resolve(event);
+
+	// Fetch the user data from the GitHub API
+	const userResponse = await fetch('https://api.github.com/user', {
+		headers: { Authorization: `Bearer ${accessToken}` }
+	});
+
+	// the user data contains things like the user's name, full name and avatar
+	const userData = await userResponse.json();
+
+	// fetch the user's email from GitHub API
+	const emailResponse = await fetch('https://api.github.com/user/emails', {
+		headers: { Authorization: `Bearer ${accessToken}` }
+	});
+
+	// the email data contains the user's emailaddressess and whether they are verified etc.
+	const emails = await emailResponse.json();
+
+	const primaryEmail = emails.find((email) => email.primary && email.verified)?.email || '';
+
+	event.locals.user = {
+		name: userData.login,
+		email: primaryEmail,
+		avatar: userData.avatar_url,
+		has_setup_payment: false
+	};
 
 	return await resolve(event);
 };
