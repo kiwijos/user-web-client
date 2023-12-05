4f2ba22c607d7001bf089b0d018a9ee8277ac28f
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-12-05 22:28:14 +0100

    Omit comment

--- src/routes/(protected)/me/+layout.server.ts
+++ src/routes/(protected)/me/+layout.server.ts
@@ -1,7 +1,6 @@
 import type { LayoutServerLoad } from './$types';
 import { redirect } from '@sveltejs/kit';
 
-// pass `locals.user` to the `page` store for use inside client-side code
 export const load: LayoutServerLoad = async ({ locals }) => {
 	if (locals.user || process.env.NODE_ENV === 'test') return;
 
