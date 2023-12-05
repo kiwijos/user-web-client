53962cf80f0777cc7e92ab54879739e41d518968
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-12-05 20:28:40 +0100

    Import and pass the required floating-ui modules to Skeleton

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
-    import "../app.pcss";
-    import { autoModeWatcher } from '@skeletonlabs/skeleton';
+	import '../app.pcss';
+	import { autoModeWatcher } from '@skeletonlabs/skeleton';
 	import { initializeStores } from '@skeletonlabs/skeleton';
+	import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
 
-    initializeStores();
+	initializeStores();
 </script>
+
 <svelte:head>
-    {@html `<script>${autoModeWatcher.toString()} autoModeWatcher();</script>`}
+	{@html `<script>${autoModeWatcher.toString()} autoModeWatcher();</script>`}
 </svelte:head>
-<slot></slot>
+<slot />
