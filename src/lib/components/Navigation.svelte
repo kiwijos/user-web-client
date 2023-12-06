c2015ff947ce9ae154fb3134be61d965330eb87b
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-12-06 11:56:22 +0100

    Add paths and adjust style slightly with added prop

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
 	import { getDrawerStore } from '@skeletonlabs/skeleton';
 	import { page } from '$app/stores';
-	import { enhance } from '$app/forms';
-	import { Avatar } from '@skeletonlabs/skeleton';
+
+	export let listLayout: string = '';
 
 	const paths = [
 		{ name: 'Dashboard', path: '/me/dashboard' },
-		{ name: 'Map', path: '/me/map' }
+		{ name: 'Account', path: '/me/account' },
+		{ name: 'Safety', path: '/me/safety' },
+		{ name: 'Parking', path: '/me/parking' }
 	];
 
 	const drawerStore = getDrawerStore();
@@ -16,32 +18,23 @@
 	}
 </script>
 
-<nav class="flex flex-col space-y-8 p-4">
-	{#if $page.data.user}
-		<div class="flex flex-col items-center justify-center space-y-2">
-			<Avatar
-				initials={$page.data.user.email[0]}
-				border="border-4 border-surface-300-600-token hover:!border-primary-500"
-				cursor="cursor-pointer"
-			/>
-			<p class="font-bold text-primary-500">{$page.data.user.email}</p>
-		</div>
-		<form action="/logout" method="POST" use:enhance>
-			<button type="submit" class="w-full btn variant-ghost">Log out</button>
-		</form>
-		<ul>
-			{#each paths as { name, path }}
-				{@const active = $page.url.pathname === path ? 'page' : null}
-				{@const activeClass =
-					$page.url.pathname === path
-						? 'btn w-full justify-start variant-soft-secondary '
-						: 'btn w-full justify-start hover:variant-soft-secondary'}
-				<li>
-					<a class={activeClass} aria-current={active} href={path} on:click={drawerClose}>{name}</a>
-				</li>
-			{/each}
-		</ul>
-	{:else}
-		<a href="/login" class="btn btn-lg variant-filled-primary">Sign in</a>
-	{/if}
-</nav>
+<ul class={listLayout}>
+	{#each paths as { name, path }}
+		{@const active = $page.url.pathname === path ? 'page' : null}
+		{@const activeClass =
+			$page.url.pathname === path ? '!variant-soft-primary ' : 'hover:!variant-soft-primary'}
+		<li>
+			<a class="btn btn-sm {activeClass}" aria-current={active} href={path} on:click={drawerClose}
+				>{name}</a
+			>
+		</li>
+	{/each}
+	<li>
+		<a
+			href="/faq"
+			class="btn btn-sm text-white bg-gradient-to-br variant-gradient-tertiary-primary"
+		>
+			Pricing &amp; FAQ ✨
+		</a>
+	</li>
+</ul>
