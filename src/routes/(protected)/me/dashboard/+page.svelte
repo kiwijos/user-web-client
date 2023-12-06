70d0275d71631684bdaef7363f528b64fb6e5889
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-12-06 15:50:47 +0100

    Display card with link to payment setup

--- src/routes/(protected)/me/dashboard/+page.svelte
+++ src/routes/(protected)/me/dashboard/+page.svelte
@@ -1,4 +1,18 @@
 <script lang="ts">
-	// import type { PageData } from './$types';
-	// export let data: PageData;
+	import { page } from '$app/stores';
 </script>
+
+{#if $page.data.user.has_setup_payment === false}
+	<a
+		href="/me/account/settings/payment"
+		class="block card card-hover p-4 space-y-4 text-white dark:text-primary-100 bg-gradient-to-br variant-gradient-tertiary-primary dark:from-tertiary-800 dark:to-primary-800"
+	>
+		<h2 class="h2 font-bold">Lås upp ditt första åk! 💫</h2>
+		<p>
+			För att hyra din första cykel behöver du ange ett betalsätt. Det gör du enkelt genom att
+			klicka på denna ruta. Oförglömliga åk väntar!
+		</p>
+	</a>
+{:else}
+	(TODO: Add dashboard content)
+{/if}
