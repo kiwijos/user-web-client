42ff326180a1d0efa207d754f7b92a6806fe3869
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-12-06 15:31:30 +0100

    Do minor style adjustments and change display language to swedish

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -3,8 +3,13 @@
 	import { enhance } from '$app/forms';
 	import { focusTrap } from '@skeletonlabs/skeleton';
 	import { PUBLIC_GITHUB_ID } from '$env/static/public';
+	// import githubLogo from '$lib/assets/icons/github-mark.svg';
+	// import githubLogoWhite from '$lib/assets/icons/github-mark-white.svg';
+	import GithubMark from '$lib/components/GithubMark.svelte';
+	// export let form: ActionData;
 
-	export let form: ActionData;
+	// pick the logo based on the theme
+	// let logo = window.document.body.classList.contains('dark-mode') ? githubLogoWhite : githubLogo;
 
 	let isFocused: boolean = true;
 
@@ -12,54 +17,55 @@
 </script>
 
 <div
-	class="flex flex-col justify-center md:max-w-sm max-w-full h-full dark:variant-glass-surface p-4 space-y-8"
+	class="flex flex-col justify-center md:max-w-sm max-w-full h-full bg-surface-100 dark:variant-glass-surface p-4 space-y-8"
 >
-	<h2 class="h2 font-thin">Log in to your account</h2>
+	<h2 class="h2 font-thin">Logga in på ditt konto</h2>
 	<p class="font-bold">
-		Don't have an account? <a href="/register" class="anchor">Create an account</a>
+		Har du inget konto? <a href="/register" class="anchor">Skapa ett konto</a>
 	</p>
 	<div class="flex flex-col space-y-4">
 		<a
-			class="block card card-hover p-4 dark:variant-ghost-surface flex items-center justify-center space-x-2"
+			class="block card card-hover p-4 border border-surface-700 flex items-center justify-center space-x-2"
 			href={github_url}
 		>
-			<span>GitHub</span>
+			<GithubMark />
+			<span>Logga in med GitHub</span>
 		</a>
 	</div>
 	<div class="inline-flex items-center justify-between w-full">
-		<hr class="w-12" />
-		<span class="px-3 text-gray-500 dark:text-gray-400">Or with email and password</span>
-		<hr class="w-12" />
+		<hr class="w-16" />
+		<span class="px-3 text-gray-500 dark:text-gray-400">Eller med mail och lösenord</span>
+		<hr class="w-16" />
 	</div>
 	<form
 		use:focusTrap={isFocused}
 		action="?/login"
 		method="POST"
 		use:enhance
-		class="flex flex-col space-y-4"
+		class="flex flex-col space-y-2"
 	>
 		<label class="label">
-			<span>Your email</span>
+			<span>Din mail</span>
 			<input
 				class="input"
 				title="Input (email)"
 				type="email"
-				placeholder="example@email.com"
+				placeholder="exempel@email.com"
 				name="email"
 			/>
 			<p>&nbsp;</p>
 		</label>
 		<label class="label">
-			<span>Your password</span>
+			<span>Ditt lösenord</span>
 			<input
 				class="input"
 				title="Input (password)"
 				type="password"
-				placeholder="password"
+				placeholder="lösenord"
 				name="password"
 			/>
 			<p>&nbsp;</p>
 		</label>
-		<button class="btn bg-primary-500" type="submit">Login</button>
+		<button class="btn bg-primary-500 text-white" type="submit">Logga in</button>
 	</form>
 </div>
--- src/routes/(auth)/register/+page.svelte
+++ src/routes/(auth)/register/+page.svelte
@@ -25,17 +25,17 @@
 </script>
 
 <div class="flex flex-col justify-center max-w-sm h-full dark:variant-glass-surface p-4 space-y-8">
-	<h2 class="h2 font-thin">Create a new account</h2>
-	<p class="font-bold">Already have an account? <a href="/login" class="anchor">Login here</a></p>
+	<h2 class="h2 font-thin">Skapa ett nytt konto</h2>
+	<p class="font-bold">Har du redan ett konto? <a href="/login" class="anchor">Logga in här</a></p>
 	<form
 		use:focusTrap={isFocused}
 		action="?/register"
 		method="POST"
-		class="flex flex-col space-y-4"
+		class="flex flex-col space-y-2"
 		use:enhance
 	>
 		<label class="label">
-			<span>What should we call you?</span>
+			<span>Vad ska vi kalla dig?</span>
 			<input
 				class="input"
 				title="Input (text)"
@@ -46,34 +46,36 @@
 			<p class="text-error-400">&nbsp;{usernameError}</p>
 		</label>
 		<label class="label">
-			<span>Your email</span>
+			<span>Din mail</span>
 			<input
 				class="input"
 				title="Input (email)"
 				type="email"
-				placeholder="example@email.com"
+				placeholder="exempel@email.com"
 				autocomplete="email"
 				name="email"
 			/>
 			<p class="text-error-400">&nbsp;{emailError}</p>
 		</label>
 		<label class="label">
-			<span>Your password</span>
+			<span>Ditt lösenord</span>
 			<input
 				class="input"
 				title="Input (password)"
 				type="password"
-				placeholder="password"
+				placeholder="lösenord"
 				name="password"
 			/>
 			<p class="text-error-400">&nbsp;{passwordError}</p>
 		</label>
-		<button class="btn bg-primary-500" aria-describedby="helper-text-explanation" type="submit"
-			>Create an account</button
+		<button
+			class="btn bg-primary-500 text-white"
+			aria-describedby="helper-text-explanation"
+			type="submit">Skapa konto</button
 		>
 		<p id="helper-text-explanation" class="mt-2 text-sm text-gray-500 dark:text-gray-400">
-			We do not share your details. Read our <a href="/privacy-policy" class="font-medium anchor"
-				>Privacy Policy</a
+			Vi delar inte din data. Läs vår <a href="/privacy-policy" class="font-medium anchor"
+				>Integritetspolicy</a
 			>.
 		</p>
 	</form>
