574d43794fb19d46d379ffa664de9432d0eda72c
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-12-06 15:49:16 +0100

    Create component with GitHub mark image that updates based on theme

---
+++ src/lib/components/GithubMark.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import githubLogo from '$lib/assets/icons/github-mark.svg';
+	import githubLogoWhite from '$lib/assets/icons/github-mark-white.svg';
+	import { onMount } from 'svelte';
+
+	let logo: string;
+	onMount(() => {
+		// pick the logo color based on the theme
+		logo = document.documentElement.classList.contains('dark') ? githubLogoWhite : githubLogo;
+	});
+	export let width = 'w-6';
+	let height = width.replace('w', 'h');
+</script>
+
+<img src={logo} class="{width} {height}" alt="GitHub mark" />
