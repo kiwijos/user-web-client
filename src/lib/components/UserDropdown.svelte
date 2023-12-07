bff9e0ba770a1abccf6a37e97b03631e9e1e9b2e
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-12-07 15:04:30 +0100

    Update paths and comment out notification for now

--- src/lib/components/UserDropdown.svelte
+++ src/lib/components/UserDropdown.svelte
@@ -13,8 +13,8 @@
 	const accountLinks = [
 		{ name: 'Inställningar', path: '/me/account/settings' },
 		{
-			name: 'Betalning',
-			path: '/me/account/settings/payment'
+			name: 'Kortuppgifter',
+			path: '/me/account/payment'
 		},
 		{ name: 'Transaktioner', path: '/me/account/transactions' },
 		{ name: 'Resehistorik', path: '/me/account/trips' }
@@ -26,24 +26,27 @@
 	class="md:me-0 rounded-full focus:ring-4 focus:ring-surface-300 dark:focus:ring-surface-600 relative inline-block"
 	use:popup={popupClick}
 >
-	{#if $page.data.user.has_setup_payment === false}
+	<!-- {#if $page.data.user.has_setup_payment === false}
 		<span
 			class="badge-icon variant-filled-warning absolute -top-1 -right-1 z-10 text-white text-xs font-bold w-4 h-4"
 			>!</span
 		>
-	{/if}
+	{/if} -->
 	<Avatar width="w-8" src={$page.data.user.avatar} initials={$page.data.user.name[0]} />
 </button>
 <div data-popup="popupClick">
 	<div
 		class="my-2 mx-2 text-base list-none bg-surface-100 divide-y divide-surface-200 rounded-lg drop-shadow-sm dark:bg-surface-700 dark:divide-gray-600"
 	>
-		<div class="px-4 py-3">
+		<a
+			href="/me/account"
+			class="block rounded-t-lg px-4 py-3 hover:bg-gray-200 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+		>
 			<span class="block text-sm text-gray-900 dark:text-white">{$page.data.user.name}</span>
 			<span class="block text-sm text-gray-500 truncate dark:text-gray-400"
 				>{$page.data.user.email}</span
 			>
-		</div>
+		</a>
 		<ul class="py-2">
 			{#each accountLinks as link}
 				<li>
@@ -58,7 +61,7 @@
 		<form action="/logout" method="POST" use:enhance>
 			<button
 				type="submit"
-				class="block w-full px-4 py-2 text-sm text-left text-gray-700 hover:bg-gray-200 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+				class="block w-full rounded-b-lg px-4 py-2 text-sm text-left text-gray-700 hover:bg-gray-200 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
 			>
 				Logga ut
 			</button>
