a6d522ed8253bd6e4a6d6beca1b40afe50b65dee
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-12-07 14:57:34 +0100

    Type fa icons import

--- src/app.d.ts
+++ src/app.d.ts
@@ -16,4 +16,12 @@ declare global {
 	}
 }
 
+declare module '@fortawesome/free-solid-svg-icons/index.es' {
+	export * from '@fortawesome/free-solid-svg-icons';
+}
+
+declare module '@fortawesome/free-brands-svg-icons/index.es' {
+	export * from '@fortawesome/free-brands-svg-icons';
+}
+
 export {};
