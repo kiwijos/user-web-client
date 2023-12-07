0d9cc0334f1f3257f4defc5bedd53cf351ca8ddc
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-12-07 15:43:50 +0100

    Create account landing page

---
+++ src/routes/(protected)/me/account/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { Avatar } from '@skeletonlabs/skeleton';
+</script>
+
+<div class="flex space-x-4 items-end">
+	<Avatar src={$page.data.user.avatar} initials={$page.data.user.name[0]} />
+	<div>
+		<p class="text-2xl">{$page.data.user.name}</p>
+		<p class="text-gray-500 dark:text-gray-400">{$page.data.user.email}</p>
+	</div>
+</div>
