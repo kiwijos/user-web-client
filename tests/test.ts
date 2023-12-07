b715d7fe27211f32a7f55ad93b62388e5f0bdc5b
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-12-07 16:39:18 +0100

    Skip test as page no longer exists

--- tests/test.ts
+++ tests/test.ts
@@ -1,7 +1,7 @@
 import { expect, test } from '@playwright/test';
 
 test.describe('map page test', () => {
-	test('map page has one expected map container', async ({ page }) => {
+	test.skip('map page (no longer exists!) has one expected map container', async ({ page }) => {
 		await page.goto('/me/map');
 		const mapContainer = page.locator('.map');
 		await expect(mapContainer).toBeTruthy();
