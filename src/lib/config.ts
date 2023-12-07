39b57009e79220a80ba35c7696719bf104a87a46
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-12-07 14:56:31 +0100

    Export app details for easy access and consistent use across all project files

---
+++ src/lib/config.ts
@@ -0,0 +1,8 @@
+import { dev } from '$app/environment';
+
+export const brand = 'R.:iders';
+export const icon = '.:i';
+export const owner = 'Riders Developers';
+export const description = 'SvelteKit app';
+export const url = dev ? 'http://localhost:5173/' : '';
+export const github = 'https://github.com/kiwijos/';
