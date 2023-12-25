03436360772c286ca1fb87ba861c6fc8ebab903c
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-12-25 21:52:47 +0100

    Place links used accross multiple files here for consistency and to maintain easier

---
+++ src/lib/utils/links.ts
@@ -0,0 +1,23 @@
+import {
+	faCog,
+	faCreditCard,
+	faMapMarkerAlt,
+	faWallet,
+	faUser
+} from '@fortawesome/free-solid-svg-icons';
+
+export const accountLinks = [
+	{ name: 'Profil', path: '/me/account' },
+	{ name: 'Inställningar', path: '/me/account/settings' },
+	{ name: 'Kortuppgifter', path: '/me/account/payment' },
+	{ name: 'Betalningar', path: '/me/account/transactions' },
+	{ name: 'Resehistorik', path: '/me/account/trips' }
+];
+
+export const accountLinksWithIcons = [
+	{ name: 'Profil', path: '/me/account', icon: faUser },
+	{ name: 'Inställningar', path: '/me/account/settings', icon: faCog },
+	{ name: 'Kortuppgifter', path: '/me/account/payment', icon: faCreditCard },
+	{ name: 'Betalningar', path: '/me/account/transactions', icon: faWallet },
+	{ name: 'Resehistorik', path: '/me/account/trips', icon: faMapMarkerAlt }
+];
