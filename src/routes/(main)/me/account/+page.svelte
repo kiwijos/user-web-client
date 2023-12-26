5b4dad60fb58978945f571f0a9c0d36da29009cd
kiwijos <allq22@student.bth.se>
2023-12-26 23:00:07 +0100

    Change from flex to grid, also move to (main)

---
+++ src/routes/(main)/me/account/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { Avatar } from '@skeletonlabs/skeleton';
+</script>
+
+<div class="grid grid-cols-4">
+	<div class="col-span-3">
+		<div class="flex space-x-4 items-start">
+			<Avatar src={$page.data.user.avatar} initials={$page.data.user.name[0]} />
+			<div>
+				<p class="text-2xl">{$page.data.user.name}</p>
+				<p class="text-gray-500 dark:text-gray-400">{$page.data.user.email}</p>
+			</div>
+		</div>
+	</div>
+</div>
