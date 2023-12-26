72695f45c1c563af7cf8cbdc60df3c50f274b8e4
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-12-26 14:09:54 +0100

    Add support for text shadow classes via a custom tailwind plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import { join } from 'path';
 import type { Config } from 'tailwindcss';
 import forms from '@tailwindcss/forms';
+import plugin from 'tailwindcss/plugin';
 
 // 1. Import the Skeleton plugin
 import { skeleton } from '@skeletonlabs/tw-plugin';
@@ -14,9 +15,25 @@ const config: Config = {
 		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
 	],
 	theme: {
-		extend: {}
+		extend: {
+			textShadow: {
+				sm: '0 1px 2px var(--tw-shadow-color)',
+				DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+				lg: '0 8px 16px var(--tw-shadow-color)'
+			}
+		}
 	},
 	plugins: [
+		plugin(function ({ matchUtilities, theme }) {
+			matchUtilities(
+				{
+					'text-shadow': (value) => ({
+						textShadow: value
+					})
+				},
+				{ values: theme('textShadow') }
+			);
+		}),
 		forms,
 		// 4. Append the Skeleton plugin (after other plugins)
 		skeleton({
