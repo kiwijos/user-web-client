3c8f87444ca0abd2149243480314004f0895b531
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-12-26 14:08:59 +0100

    Declare regular icons import

--- src/app.d.ts
+++ src/app.d.ts
@@ -20,6 +20,10 @@ declare module '@fortawesome/free-solid-svg-icons/index.es' {
 	export * from '@fortawesome/free-solid-svg-icons';
 }
 
+declare module '@fortawesome/free-regular-svg-icons/index.es' {
+	export * from '@fortawesome/free-regular-svg-icons';
+}
+
 declare module '@fortawesome/free-brands-svg-icons/index.es' {
 	export * from '@fortawesome/free-brands-svg-icons';
 }
