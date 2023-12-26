27c9b2a65d67fd74b7a0058cca43e70790adda50
kiwijos <allq22@student.bth.se>
2023-12-26 22:56:35 +0100

    Center content using margin auto, also move file to (main)

--- src/routes/(main)/blog/[slug]/+page.svelte
+++ src/routes/(main)/blog/[slug]/+page.svelte
@@ -4,4 +4,6 @@
 	export let data: PageData;
 </script>
 
-<h1 class="h1 capitalize">{data.title}</h1>
+<div class="mx-auto max-w-4xl p-4 md:p-8">
+	<h1 class="h1 capitalize">{data.title}</h1>
+</div>
