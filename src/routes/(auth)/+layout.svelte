4dd55e449ea680a311f877e34cb12d6364779f88
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-12-26 21:32:23 +0100

    Create a gradient background for login and register pages

---
+++ src/routes/(auth)/+layout.svelte
@@ -0,0 +1,5 @@
+<div
+	class="h-full w-full bg-gradient-to-br text-white dark:text-primary-100 from-primary-500 via-tertiary-300 to-secondary-400 dark:from-tertiary-800 dark:via-tertiary-900 dark:to-primary-800"
+>
+	<slot />
+</div>
