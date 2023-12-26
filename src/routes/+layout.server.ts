1a46730756654ae251450d24e6d1d14895b41c16
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-12-26 14:13:23 +0100

    Use the optional chaining (?.) operator with the nullish coalescing operator (??) to set the user object to null when not present in the request

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,6 +3,6 @@ import type { LayoutServerLoad } from './$types';
 // pass `locals.user` to the `page` store for use inside client-side code
 export const load: LayoutServerLoad = async ({ locals }) => {
 	return {
-		user: locals.user
+		user: locals?.user ?? null
 	};
 };
