bf56aa7a5cdc35e52f65e6b20e68cbf487e02c08
kiwijos <allq22@student.bth.se>
2024-01-03 11:44:00 +0100

    Create page to render errors

---
+++ src/routes/(auth)/+error.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<h2 class="h2 font-thin text-surface-700 dark:text-surface-300">
+	{$page.status}: {$page.error?.message}
+</h2>
+<p class="font-medium text-surface-700 dark:text-surface-300">
+	Har du ett konto? Försök igen. <a href="/login" class="anchor">Logga in</a>
+</p>
+
+<p class="font-medium text-surface-700 dark:text-surface-300">
+	Eller har du bara hamnat fel? <a href="/" class="anchor">Startsidan</a>
+</p>
