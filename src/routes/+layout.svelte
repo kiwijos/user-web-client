6ae11f297a9b0efbcc3bf5a29701edd7f4ed8242
kiwijos <allq22@student.bth.se>
2024-01-03 11:52:13 +0100

    Put lightswitch in the root layout for easy access during development

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import '../app.pcss';
-	import { autoModeWatcher } from '@skeletonlabs/skeleton';
+	import { autoModeWatcher, LightSwitch } from '@skeletonlabs/skeleton';
 	import { initializeStores } from '@skeletonlabs/skeleton';
 	import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
@@ -13,3 +13,6 @@
 	{@html `<script>${autoModeWatcher.toString()} autoModeWatcher();</script>`}
 </svelte:head>
 <slot />
+<div class="fixed bottom-0 right-0 z-50 p-4">
+	<LightSwitch />
+</div>
