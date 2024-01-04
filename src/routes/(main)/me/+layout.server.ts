0ad6f013be90465de7416ce78b4ef6a536c78755
kiwijos <allq22@student.bth.se>
2024-01-04 14:21:37 +0100

    Remove needless code, auth check is handled in hook instead

--- src/routes/(main)/me/+layout.server.ts
+++
@@ -1,8 +0,0 @@
-import type { LayoutServerLoad } from './$types';
-import { redirect } from '@sveltejs/kit';
-
-export const load: LayoutServerLoad = async ({ locals }) => {
-	if (locals.user || process.env.NODE_ENV === 'test') return;
-
-	throw redirect(302, '/login');
-};
