f48046429c675623839fee4d73c720fe279b4330
kiwijos <allq22@student.bth.se>
2024-01-07 00:03:03 +0100

    Create error page with status code for use during development

---
+++ src/routes/(main)/me/+error.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<h2 class="h2 font-thin text-surface-700 dark:text-surface-300">
+	{$page.status}
+	{$page.error?.message}
+</h2>
