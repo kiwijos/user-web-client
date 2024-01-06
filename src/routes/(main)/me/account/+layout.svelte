fce78ef1533492787e64aff349eb3c0d13d929cf
kiwijos <allq22@student.bth.se>
2024-01-06 22:10:35 +0100

    Layout using grid instead of flex. Also, make crumbs span entire first row.

--- src/routes/(main)/me/account/+layout.svelte
+++ src/routes/(main)/me/account/+layout.svelte
@@ -4,18 +4,18 @@
 </script>
 
 <div
-	class="flex drop-shadow-xl rounded-container-token bg-surface-50 dark:bg-surface-900 border border-surface-200 dark:border-surface-700"
+	class="grid grid-cols-4 drop-shadow-xl rounded-container-token bg-surface-50 dark:bg-surface-900 border border-surface-200 dark:border-surface-700"
 >
-	<div class="grow">
-		<div class="px-4 md:px-8 pb-4 pt-8 flex items-center">
-			<Breadcrumb />
-		</div>
-		<div class="px-4 md:px-8 pb-8 pt-4">
-			<slot />
-		</div>
+	<div
+		class="col-span-4 px-4 md:px-8 pb-4 pt-8 flex items-center bg-surface-100 dark:bg-surface-900 border-b border-surface-200 dark:border-surface-700 rounded-t-md"
+	>
+		<Breadcrumb />
+	</div>
+	<div class="col-span-4 sm:col-span-3 px-4 md:px-8 pb-8 pt-4">
+		<slot />
 	</div>
 	<div
-		class="py-8 shrink-0 bg-surface-100 dark:bg-surface-900 border-0 border-l border border-surface-200 dark:border-surface-700 hidden md:block w-56 rounded-r-md"
+		class="col-span-1 bg-surface-50 dark:bg-surface-900 border-0 border-l border border-surface-200 dark:border-surface-700 hidden sm:block rounded-br-md"
 	>
 		<UserNav />
 	</div>
