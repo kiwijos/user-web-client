7cde9b59f2e0401455f64a6ebb2bbd8bd919467a
kiwijos <allq22@student.bth.se>
2024-01-06 14:26:22 +0100

    Add single instance of the toast component to be used throughout
    The component uses the singleton pattern

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import '../app.pcss';
 	import { autoModeWatcher, LightSwitch } from '@skeletonlabs/skeleton';
+	import { Toast } from '@skeletonlabs/skeleton';
 	import { initializeStores } from '@skeletonlabs/skeleton';
 	import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
@@ -12,6 +13,7 @@
 <svelte:head>
 	{@html `<script>${autoModeWatcher.toString()} autoModeWatcher();</script>`}
 </svelte:head>
+<Toast />
 <slot />
 <div class="fixed bottom-0 right-0 z-50 p-4">
 	<LightSwitch />
