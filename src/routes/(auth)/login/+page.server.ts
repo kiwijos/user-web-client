f0603f6b0b25ef91517a96a49e2ea65d94227805
kiwijos <allq22@student.bth.se>
2024-01-07 09:59:22 +0100

    Remove old and unused code

--- src/routes/(auth)/login/+page.server.ts
+++ src/routes/(auth)/login/+page.server.ts
@@ -1,14 +1,7 @@
 import { fail, redirect } from '@sveltejs/kit';
-import type { Action, Actions, PageServerLoad } from './$types';
+import type { Action, Actions } from './$types';
 
-export const load: PageServerLoad = async ({ locals }) => {
-	if (locals.user) {
-		// redirect logged in users to the dashboard
-		throw redirect(302, '/');
-	}
-};
-
-const login: Action = async ({ cookies, request }) => {
+const login: Action = async ({ request }) => {
 	const data = await request.formData();
 
 	const email = data.get('email');
@@ -17,12 +10,6 @@ const login: Action = async ({ cookies, request }) => {
 	if (typeof email !== 'string' || typeof password !== 'string' || !email || !password) {
 		return fail(400, { invalid: true });
 	}
-
-	cookies.set('session', email, {
-		path: '/',
-		sameSite: 'strict',
-		maxAge: 60 * 60 * 24 * 30
-	});
 
 	// redirect the user
 	throw redirect(302, '/');
