4983169d365172d9b6c8a39284c69b9bbca4c9e6
kiwijos <allq22@student.bth.se>
2024-01-15 01:06:22 +0100

    Refactor and fetch balance

--- src/routes/(main)/me/account/+layout.server.ts
+++ src/routes/(main)/me/account/+layout.server.ts
@@ -3,32 +3,30 @@ import { PUBLIC_REST_API_URL } from '$env/static/public';
 
 // pass `locals.user` to the `page` store for use inside client-side code
 export const load: LayoutServerLoad = async ({ fetch }) => {
-	let cardDetails;
+	let card;
+	let balance;
 
 	try {
 		const response = await fetch(`${PUBLIC_REST_API_URL}/user/card`, {
 			method: 'POST'
 		});
 
-		cardDetails = await response.json();
+		const result = await response.json();
 
 		if (!response.ok) {
 			// When the response is not OK, the server will return an `errors` object
-			// In the case of no card, the server will return a 404 status code (that's the plan anyway)
+			// In the case of no result, the server will return a 404 status code (that's the plan anyway)
 			let message;
-			if (cardDetails.errors?.detail) message = cardDetails.errors.detail;
-			else if (cardDetails.errors?.message) message = cardDetails.errors.message;
-			else message = String(cardDetails.errors);
+			if (result.errors?.detail) message = result.errors.detail;
+			else if (result.errors?.message) message = result.errors.message;
+			else message = String(result.errors);
 
 			console.error(
 				`Server responded with an error when requesting card details: (${response.status}) ${message}`
 			);
-
-			// This is an expected error, thrown when the user has no card for example
-			return {
-				card: undefined // <-- we don't want to return the error object to the client, simply return undefined
-			};
 		}
+
+		card = result;
 	} catch (error) {
 		// These are unexpected errors, thrown when the server is unreachable, the response cannot be parsed, etc.
 		let message;
@@ -36,13 +34,39 @@ export const load: LayoutServerLoad = async ({ fetch }) => {
 		else message = String(error);
 
 		console.error(`An unexpected error occurred when requesting card details: ${message}`);
+	}
 
-		return {
-			card: undefined
-		};
+	try {
+		const response = await fetch(`${PUBLIC_REST_API_URL}/user/balance`, {
+			method: 'POST'
+		});
+
+		const result = await response.json();
+
+		if (!response.ok) {
+			// When the response is not OK, the server will return an `errors` object
+			// In the case of no card, the server will return a 404 status code (that's the plan anyway)
+			let message;
+			if (result.errors?.detail) message = result.errors.detail;
+			else if (result.errors?.message) message = result.errors.message;
+			else message = String(result.errors);
+
+			console.error(
+				`Server responded with an error when requesting balance: (${response.status}) ${message}`
+			);
+		}
+		balance = result?.balance;
+	} catch (error) {
+		// These are unexpected errors, thrown when the server is unreachable, the response cannot be parsed, etc.
+		let message;
+		if (error instanceof Error) message = error.message;
+		else message = String(error);
+
+		console.error(`An unexpected error occurred when requesting balance: ${message}`);
 	}
 
 	return {
-		card: cardDetails
+		card,
+		balance
 	};
 };
