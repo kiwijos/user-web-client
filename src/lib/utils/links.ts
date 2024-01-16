68d62d57bd6e65f2175dc59cc96c40ebe193b1f9
kiwijos <allq22@student.bth.se>
2024-01-16 11:37:18 +0100

    Add link to prepay page

--- src/lib/utils/links.ts
+++ src/lib/utils/links.ts
@@ -4,13 +4,15 @@ import {
 	// faMapMarkerAlt,
 	faRoute,
 	faWallet,
-	faUser
+	faUser,
+	faMoneyBillTransfer
 } from '@fortawesome/free-solid-svg-icons';
 
 export const accountLinks = [
 	{ name: 'Profil', path: '/me/account' },
 	{ name: 'Inställningar', path: '/me/account/settings' },
 	{ name: 'Kortuppgifter', path: '/me/account/payment' },
+	{ name: 'Prepay', path: '/me/account/prepay' },
 	{ name: 'Betalningar', path: '/me/account/transactions' },
 	{ name: 'Resehistorik', path: '/me/account/trips' }
 ];
@@ -19,6 +21,7 @@ export const accountLinksWithIcons = [
 	{ name: 'Profil', path: '/me/account', icon: faUser },
 	{ name: 'Inställningar', path: '/me/account/settings', icon: faCog },
 	{ name: 'Kortuppgifter', path: '/me/account/payment', icon: faCreditCard },
+	{ name: 'Prepay', path: '/me/account/prepay', icon: faMoneyBillTransfer },
 	{ name: 'Betalningar', path: '/me/account/transactions', icon: faWallet },
 	{ name: 'Resehistorik', path: '/me/account/trips', icon: faRoute }
 ];
