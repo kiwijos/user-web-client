2b1e3e86cbdd99a9827c25246a57296e54d2af97
kiwijos <allq22@student.bth.se>
2024-01-16 11:14:23 +0100

    Register custom table modal

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
 	import '../app.pcss';
 	import { autoModeWatcher, LightSwitch } from '@skeletonlabs/skeleton';
-	import { Toast } from '@skeletonlabs/skeleton';
+	import { Toast, Modal, type ModalComponent } from '@skeletonlabs/skeleton';
+	import ModalTable from '$lib/components/ModalTable.svelte';
+
 	import { initializeStores } from '@skeletonlabs/skeleton';
 	import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
+
+	const modalRegistry: Record<string, ModalComponent> = {
+		// Pass the component reference
+		modalTable: { ref: ModalTable }
+	};
 
 	initializeStores();
 </script>
@@ -13,6 +20,7 @@
 <svelte:head>
 	{@html `<script>${autoModeWatcher.toString()} autoModeWatcher();</script>`}
 </svelte:head>
+<Modal components={modalRegistry} />
 <Toast />
 <slot />
 <div class="fixed bottom-0 right-0 z-50 p-4">
